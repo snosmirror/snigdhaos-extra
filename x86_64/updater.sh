f7fd3264551d15c22fb4b9dc8000aa3b4f5093ea
Eshan Roy <m.eshanized@gmail.com>
2024-03-19 02:56:07 +0530

    Add pkgs to snigdhaos-extra

---
+++ x86_64/updater.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author : Eshan Roy
+# Author URI : https://github.com/eshanized
+
+rm snigdhaos-extra*
+
+echo "Repo Adding..."
+repo-add -s -n -R snigdhaos-extra.db.tar.gz *.pkg.tar.zst
+
+sleep 1
+
+rm snigdhaos-extra.db
+rm snigdhaos-extra.db.sig
+
+rm snigdhaos-extra.files
+rm snigdhaos-extra.files.sig
+
+mv snigdhaos-extra.db.tar.gz snigdhaos-extra.db
+mv snigdhaos-extra.db.tar.gz.sig snigdhaos-extra.db.sig
+
+mv snigdhaos-extra.files.tar.gz snigdhaos-extra.files
+mv snigdhaos-extra.files.tar.gz.sig snigdhaos-extra.files.sig
+
+
+echo "Repo Updated..."
+echo "*******************************************************************************"
